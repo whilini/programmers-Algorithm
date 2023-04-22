87472005ef7e7518a17daeb88a1274efb816f3f9
whilini <whiliniong@gmail.com>
2023-04-22 19:32:17 +0900

    완주하지 못한 선수(with new Map())

--- Level1/notFinishedPlayer.js
+++ Level1/notFinishedPlayer.js
@@ -1,18 +1,40 @@
+// function solution(participant, completion) {
+//   var answer = "";
+//   let obj = {};
+
+//   participant.forEach((p) => {
+//     obj[p] = obj[p] ? obj[p] + 1 : 1;
+//   });
+//   completion.forEach((c) => {
+//     obj[c] = obj[c] - 1;
+//   });
+
+//   Object.entries(obj).map(([k, v]) => {
+//     if (v === 1) answer = k;
+//   });
+
+//   return answer;
+// }
+
 function solution(participant, completion) {
   var answer = "";
-  let obj = {};
+  let map = new Map();
 
   participant.forEach((p) => {
-    obj[p] = obj[p] ? obj[p] + 1 : 1;
+    const already = map.get(p);
+    map.set(p, already ? already + 1 : 1);
   });
   completion.forEach((c) => {
-    obj[c] = obj[c] - 1;
+    const player = map.get(c);
+    map.set(c, player - 1);
   });
 
-  Object.entries(obj).map(([k, v]) => {
-    if (v === 1) answer = k;
+  map.forEach((v, k) => {
+    if (v === 1) {
+      console.log(k);
+      answer = k;
+    }
   });
-
   return answer;
 }
 
